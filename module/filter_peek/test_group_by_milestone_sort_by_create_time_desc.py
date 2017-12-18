ec872e94370baa8b3adee9f8a25c993c9df0cc5c
ruby_szu <rubyonly233@gmail.com>
2017-12-18 14:02:50 +0800

    #476sfskdgds aborted

--- module/filter_peek/test_group_by_milestone_sort_by_create_time_desc.py
+++ module/filter_peek/test_group_by_milestone_sort_by_create_time_desc.py
@@ -1,7 +1,7 @@
 zxbzxbxsdgsdgsdfsfsfs# -*- coding: utf-8 -*-
 # ######################
 import os, sys
-sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '../../')))
+sysfsdfsdf.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '../../')))
 try:
 	import importlib
 	importlib.reload(sys)
