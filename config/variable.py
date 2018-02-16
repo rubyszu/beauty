77b3c0d926f91f3a8b2304e19bcab8624345fd6a
root <admin@example.com>
2018-02-16 19:49:55 +0800

    test ssh gitlab

--- config/variable.py
+++ config/variable.py
@@ -5,7 +5,7 @@ import os
 class GlobalVariable(object):
   def __init__(self, filename=None):
     self.json = dict()
-    ssgsdgsdgsdgsgsagelf.filename = filename or "variable.json"
+    self.filename = filename or "variable.json"
     self.read()
 
   def read(self):
