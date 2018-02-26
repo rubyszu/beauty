32a77337fead92f8c3eb4bdb8402ed1a5820b08c
root <admin@example.com>
2018-02-26 22:30:25 +0800

    asfsfsf

--- a.sh
+++ a.sh
@@ -4,4 +4,4 @@ PATH=${PATH}:/usr/local/bin
 #pip install -U pytest
 #py.test --junitxml results.xml tests.py
 
-py.test --junitxml results.xml module/login/test_login_200.py
+cd module/login && py.test --junitxml results.xml test_login_200.py
