3a79b0a81484dc7d627729f33d6e249257dec0b3
rubyszu <rubyonly233@gmail.com>
2018-03-08 12:02:17 +0800

    1241241

--- java-example/User.java
+++ java-example/User.java
@@ -1,29 +1,29 @@
-package com.example.hello;
+// package com.example.hello;
 
-public class User {
+// public class User {
 
-	private String name;
-	private int age;
+// 	private String name;
+// 	private int age;
 	
-	public User(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
+// 	public User(String name, int age) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
 	
-	public User() {
-	}
+// 	public User() {
+// 	}
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
+// 	public String getName() {
+// 		return name;
+// 	}
+// 	public void setName(String name) {
+// 		this.name = name;
+// 	}
+// 	public int getAge() {
+// 		return age;
+// 	}
+// 	public void setAge(int age) {
+// 		this.age = age;
+// 	}
 	
-}
+// }
