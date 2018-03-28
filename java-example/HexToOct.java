4a562365ef781f4024d9274bc4588857e1c667b7
bqgparfyxia <rubyonly233@gmail.com>
2018-03-28 15:59:00 +0800

    ptyoasknfd

--- java-example/HexToOct.java
+++ java-example/HexToOct.java
@@ -1,56 +1,31 @@
 import java.util.Scanner;
 
-public class HexToOct
-{ 
+package heaps;
 
-    public static int hex2decimal(String s)
-    {
-             String str = "0123456789ABCDEF";  
-             s = s.toUpperCase();
-             int val = 0;
-             for (int i = 0; i < s.length(); i++)
-             {
-                 char a = s.charAt(i);
-                 int n = str.indexOf(a);
-                 val = 16*val + n;
-             }
-             return val;
-    } 
-	
-	
-	public static int decimal2octal(int q)
-	{
-		int now;
-		int i=1;
-		int octnum=0;
-		while(q>0)
-		{
-			now=q%8;
-			octnum=(now*(int)(Math.pow(10,i)))+octnum;
-			q/=8;
-			i++;
-		}
-		octnum/=10;
-		return octnum;
-	}
-	// Main method that gets the hex input from user and converts it into octal.
-    public static void main(String args[])
-    {
-        String hexadecnum;
-        int decnum,octalnum;
-        Scanner scan = new Scanner(System.in);
-		
-        System.out.print("Enter Hexadecimal Number : ");
-        hexadecnum = scan.nextLine();      
-        
-        // first convert hexadecimal to decimal
-        
-        decnum = hex2decimal(hexadecnum);       //Pass the string to the hex2decimal function and get the decimal form in variable decnum
-		
-        // convert decimal to octal
-        octalnum=decimal2octal(decnum);
-	System.out.println("Number in octal: "+octalnum);
- 
-        
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Heap tree where a node's key is higher than or equal to its parent's and lower than or equal
+ * to its children's.
+ * @author Nicolas Renard
+ *
+ */
+public class MaxHeap implements Heap {
+    
+    private final List<HeapElement> maxHeap;
+    
+    public MaxHeap(List<HeapElement> listElements) throws Exception {
+        maxHeap = new ArrayList<HeapElement>();
+        for (HeapElement heapElement : listElements) {
+            if (heapElement != null) insertElement(heapElement);
+            else System.out.println("Null element. Not added to heap");
+        }
+        if (maxHeap.size() == 0) System.out.println("No element has been added, empty heap.");
+        }
+    
+    // Get the element at a given index. The key for the list is equal to index value - 1
+    public HeapElement getElement(int elementIndex) {
+        if ((elementIndex <= 0) && (elementIndex > maxHeap.size())) throw new IndexOutOfBoundsException("Index out of heap range");
+        return maxHeap.get(elementIndex - 1);
     }
-}
