12720df9999df645856e62f9567e6d6cc8a93a68
ruby_szu <rubyonly233@gmail.com>
2018-03-28 12:22:31 +0800

    test

--- java-example/AnyBaseToAnyBase.java
+++ java-example/AnyBaseToAnyBase.java
@@ -3,128 +3,254 @@ import java.util.HashSet;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Class for converting from "any" base to "any" other base, when "any" means from 2-36.
- * Works by going from base 1 to decimal to base 2. Includes auxiliary method for 
- * determining whether a number is valid for a given base.
- * 
- * @author Michael Rolland
- * @version 2017.10.10
- *
- */
-public class AnyBaseToAnyBase {
-	
-	// Smallest and largest base you want to accept as valid input
-	static final int MINIMUM_BASE = 2;
-	static final int MAXIMUM_BASE = 36;
-	
-	// Driver
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String n;
-		int b1=0,b2=0;
-		while (true) {
-			try {
-				System.out.print("Enter number: ");
-				n = in.next();
-				System.out.print("Enter beginning base (between "+MINIMUM_BASE+" and "+MAXIMUM_BASE+"): ");
-				b1 = in.nextInt();
-				if (b1 > MAXIMUM_BASE || b1 < MINIMUM_BASE) {
-					System.out.println("Invalid base!");
-					continue;
-				}
-				if (!validForBase(n, b1)) {
-					System.out.println("The number is invalid for this base!");
-					continue;
-				}
-				System.out.print("Enter end base (between "+MINIMUM_BASE+" and "+MAXIMUM_BASE+"): ");
-				b2 = in.nextInt();
-				if (b2 > MAXIMUM_BASE || b2 < MINIMUM_BASE) {
-					System.out.println("Invalid base!");
-					continue;
-				}
-				break;
-			} catch (InputMismatchException e) {
-				System.out.println("Invalid input.");
-				in.next();
-			}
-		}
-		System.out.println(base2base(n, b1, b2));
-	}
-	
+class Node{
+	/** Data for the node */
+	public int data;
+	/** The Node to the left of this one */
+	public Node left;
+	/** The Node to the right of this one */
+	public Node right;
+	/** The parent of this node */
+	public Node parent;
+
 	/**
-	 * Checks if a number (as a String) is valid for a given base.
-	 */
-	public static boolean validForBase(String n, int base) {
-		char[] validDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
-				'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-				'W', 'X', 'Y', 'Z'};
-		// digitsForBase contains all the valid digits for the base given
-		char[] digitsForBase = Arrays.copyOfRange(validDigits, 0, base);
-		
-		// Convert character array into set for convenience of contains() method
-		HashSet<Character> digitsList = new HashSet();
-		for (int i=0; i<digitsForBase.length; i++)
-			digitsList.add(digitsForBase[i]);
-		
-		// Check that every digit in n is within the list of valid digits for that base.
-		for (char c : n.toCharArray()) 
-			if (!digitsList.contains(c))
-				return false;
-		
-		return true;
-	}
-	
-	/**
-	 * Method to convert any integer from base b1 to base b2. Works by converting from b1 to decimal,
-	 * then decimal to b2.
-	 * @param n The integer to be converted.
-	 * @param b1 Beginning base.
-	 * @param b2 End base.
-	 * @return n in base b2.
-	 */
-	public static String base2base(String n, int b1, int b2) {
-		// Declare variables: decimal value of n, 
-		// character of base b1, character of base b2,
-		// and the string that will be returned.
-		int decimalValue = 0, charB2;
-		char charB1;
-		String output="";
-		// Go through every character of n
-		for (int i=0; i<n.length(); i++) {
-			// store the character in charB1
-			charB1 = n.charAt(i);
-			// if it is a non-number, convert it to a decimal value >9 and store it in charB2
-			if (charB1 >= 'A' && charB1 <= 'Z') 
-				charB2 = 10 + (charB1 - 'A');
-			// Else, store the integer value in charB2
-			else 
-				charB2 = charB1 - '0';
-			// Convert the digit to decimal and add it to the
-			// decimalValue of n
-			decimalValue = decimalValue * b1 + charB2;
-		}
-		
-		// Converting the decimal value to base b2:
-		// A number is converted from decimal to another base
-		// by continuously dividing by the base and recording 
-		// the remainder until the quotient is zero. The number in the
-		// new base is the remainders, with the last remainder
-		// being the left-most digit.
-		
-		// While the quotient is NOT zero:
-		while (decimalValue != 0) {
-			// If the remainder is a digit < 10, simply add it to
-			// the left side of the new number.
-			if (decimalValue % b2 < 10) 
-				output = Integer.toString(decimalValue % b2) + output;
-			// If the remainder is >= 10, add a character with the
-			// corresponding value to the new number. (A = 10, B = 11, C = 12, ...)
-			else
-				output = (char)((decimalValue % b2)+55) + output;
-			// Divide by the new base again
-			decimalValue /= b2;
-		}
-		return output;
+	* Constructor of Node
+	*
+	* @param value Value to put in the node
+	*/
+	public Node(int value){
+		data = value;
+		left = null;
+		right = null;
+		parent = null;
 	}
 }
+
+
+/**
+* A binary tree is a data structure in which an element
+* has two successors(children). The left child is usually
+* smaller than the parent, and the right child is usually
+* bigger.
+*
+* @author Unknown
+*
+*/
+class Tree{
+	/** The root of the Binary Tree */
+	private Node root;
+
+	/**
+	* Constructor
+	*/
+	public Tree(){
+		root = null;
+	}
+
+	/**
+	* Method to find a Node with a certain value
+	*
+	* @param key Value being looked for
+	* @return The node if it finds it, otherwise returns the parent
+	*/
+	public Node find(int key) {
+		Node current = root;
+		while (current != null) {
+			if(key < current.data) {
+				current = current.left;
+			} else if(key > current.data) {
+				current = current.right;
+			} else {	// If you find the value return it
+				return current;
+			}
+		}
+		return null;
+	}
+
+	/**
+	* Inserts certain value into the Binary Tree
+	*
+	* @param value Value to be inserted
+	*/
+	public void put(int value){
+		Node newNode = new Node(value);
+		if(root == null)
+		root = newNode;
+		else{
+			//This will return the soon to be parent of the value you're inserting
+			Node parent = find(value);
+
+			//This if/else assigns the new node to be either the left or right child of the parent
+			if(value < parent.data){
+				parent.left = newNode;
+				parent.left.parent = parent;
+				return;
+			}
+			else{
+				parent.right = newNode;
+				parent.right.parent = parent;
+				return;
+			}
+		}
+	}
+
+	/**
+	* Deletes a given value from the Binary Tree
+	*
+	* @param value Value to be deleted
+	* @return If the value was deleted
+	*/
+	public boolean remove(int value){
+		//temp is the node to be deleted
+		Node temp = find(value);
+
+		//If the value doesn't exist
+		if(temp.data != value)
+		return false;
+
+		//No children
+		if(temp.right == null && temp.left == null){
+			if(temp == root)
+			root = null;
+
+			//This if/else assigns the new node to be either the left or right child of the parent
+			else if(temp.parent.data < temp.data)
+			temp.parent.right = null;
+			else
+			temp.parent.left = null;
+			return true;
+		}
+
+		//Two children
+		else if(temp.left != null && temp.right != null){
+			Node successor = findSuccessor(temp);
+
+			//The left tree of temp is made the left tree of the successor
+			successor.left = temp.left;
+			successor.left.parent = successor;
+
+			//If the successor has a right child, the child's grandparent is it's new parent
+			if(successor.right != null && successor.parent != temp){
+				successor.right.parent = successor.parent;
+				successor.parent.left = successor.right;
+				successor.right = temp.right;
+				successor.right.parent = successor;
+			}
+			if(temp == root){
+				successor.parent = null;
+				root = successor;
+				return true;
+			}
+
+			//If you're not deleting the root
+			else{
+				successor.parent = temp.parent;
+
+				//This if/else assigns the new node to be either the left or right child of the parent
+				if(temp.parent.data < temp.data)
+				temp.parent.right = successor;
+				else
+				temp.parent.left = successor;
+				return true;
+			}
+		}
+		//One child
+		else{
+			//If it has a right child
+			if(temp.right != null){
+				if(temp == root){
+					root = temp.right; return true;}
+
+					temp.right.parent = temp.parent;
+
+					//Assigns temp to left or right child
+					if(temp.data < temp.parent.data)
+					temp.parent.left = temp.right;
+					else
+					temp.parent.right = temp.right;
+					return true;
+				}
+				//If it has a left child
+				else{
+					if(temp == root){
+						root = temp.left; return true;}
+
+						temp.left.parent = temp.parent;
+
+						//Assigns temp to left or right side
+						if(temp.data < temp.parent.data)
+						temp.parent.left = temp.left;
+						else
+						temp.parent.right = temp.left;
+						return true;
+					}
+				}
+			}
+
+			/**
+			* This method finds the Successor to the Node given.
+			* Move right once and go left down the tree as far as you can
+			*
+			* @param n Node that you want to find the Successor of
+			* @return The Successor of the node
+			*/
+			public Node findSuccessor(Node n){
+				if(n.right == null)
+				return n;
+				Node current = n.right;
+				Node parent = n.right;
+				while(current != null){
+					parent = current;
+					current = current.left;
+				}
+				return parent;
+			}
+
+			/**
+			* Returns the root of the Binary Tree
+			*
+			* @return the root of the Binary Tree
+			*/
+			public Node getRoot(){
+				return root;
+			}
+
+			/**
+			* Prints leftChild - root - rightChild
+			*
+			* @param localRoot The local root of the binary tree
+			*/
+			public void inOrder(Node localRoot){
+				if(localRoot != null){
+					inOrder(localRoot.left);
+					System.out.print(localRoot.data + " ");
+					inOrder(localRoot.right);
+				}
+			}
+
+			/**
+			* Prints root - leftChild - rightChild
+			*
+			* @param localRoot The local root of the binary tree
+			*/
+			public void preOrder(Node localRoot){
+				if(localRoot != null){
+					System.out.print(localRoot.data + " ");
+					preOrder(localRoot.left);
+					preOrder(localRoot.right);
+				}
+			}
+
+			/**
+			* Prints rightChild - leftChild - root
+			*
+			* @param localRoot The local root of the binary tree
+			*/
+			public void postOrder(Node localRoot){
+				if(localRoot != null){
+					postOrder(localRoot.left);
+					postOrder(localRoot.right);
+					System.out.print(localRoot.data + " ");
+				}
+			}
+		}
