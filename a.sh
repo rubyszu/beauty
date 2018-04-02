41c021bb5416db5623ef0d68505fa6932f61fb93
rubyszu <rubyonly233@gmail.com>
2018-04-02 23:11:28 +0800

    test

--- a.sh
+++ a.sh
@@ -2,6 +2,8 @@
 PATH=${PATH}:/usr/local/bin
 
 #pip install -U pytest
-py.test --junitxml results.xml tests.py
+# py.test --junitxml results.xml tests.py
+
+cd module && python testsuit.py
 
 # cd module/login && py.test --junitxml results.xml test_login_200.py
