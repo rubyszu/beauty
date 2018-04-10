2dbf541bfa3bb186589128344d790bf2ec6421be
rubyszu <rubyonly233@gmail.com>
2018-04-10 16:24:47 +0800

    asfsaf

--- pmd-example/run.sh
+++ pmd-example/run.sh
@@ -1,5 +1,14 @@
 # ~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R rulesets/java/unusedcode.xml -f text -r unusedcode.txt
 
-# ~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R unusedcode.xml -f text -r unusedcode.txt
+# ~/pmd-bin-6.0.0/bin/run.sh pmd -d ./src/HelloWorld.java -R unusedcode.xml -f text -r unusedcode.txt
 
-~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R unusedcode.xml -f xml -r violation.xml
+# ~/pmd-bin-6.0.0/bin/run.sh pmd -d ../../lucky/  -R unusedcode.xml -f text -r unusedcode.txt
+
+~/pmd-bin-6.0.0/bin/run.sh pmd -d ../../lucky/  -R unusedcode.xml -f xml -r violation.xml
+
+
+# ~/pmd-bin-6.0.0/bin/run.sh pmd -d AVLTree.java -R unusedcode.xml -f xml -r violation.xml
+
+# cpd
+# ~/pmd-bin-6.0.0/bin/run.sh cpd --minimum-tokens 100 --language php --files ../pmd-master --encoding utf-8 --format xml  > cpd.xml
+# ~/pmd-bin-6.0.0/bin/run.sh cpd --minimum-tokens 100 --language php --files src --encoding utf-8 --format xml  > cpd.xml
