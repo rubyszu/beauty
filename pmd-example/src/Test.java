c15a98226c3cd466eff9202ff04141ab807ce118
pgqypgqypgqy <rubyonly233@gmail.com>
2018-04-12 17:15:50 +0800

    asfjasf

--- pmd-example/src/Test.java
+++ pmd-example/src/Test.java
@@ -1,5 +1,8 @@
-public void doSomething() {
+public class HelloPython {
+	public void doSomething() {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     // 98 copies omitted for brevity.
+	}
+
 }
