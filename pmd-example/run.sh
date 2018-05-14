cd42157235957d83799e9fc4f6000041feb76e71
Lei Yang <rubyonly233@gmail.com>
2018-05-14 15:54:47 +0800

    test

--- pmd-example/run.sh
+++
@@ -1,5 +0,0 @@
-# ~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R rulesets/java/unusedcode.xml -f text -r unusedcode.txt
-
-# ~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R unusedcode.xml -f text -r unusedcode.txt
-
-~/pmd-bin-6.0.0/bin/run.sh pmd -d src -R unusedcode.xml -f xml -r violation.xml
