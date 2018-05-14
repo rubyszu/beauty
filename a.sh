0e2c4bf990bd997544b05f63877dc9224c98fcc4
Ruby <rubyonly233@gmail.com>
2018-05-14 16:02:27 +0800

    feature:#71 屏蔽邀请成员的入口（包括右上角邀请成员；团队配置中心 - 成员名单；新建项目）

--- a.sh
+++ a.sh
@@ -4,7 +4,8 @@ PATH=${PATH}:/usr/local/bin
 #pip install -U pytest
 # py.test --junitxml results.xml tests.py
 
-# cd module/test && python testsuit.py
+cd module/test && python testsuit.py
+cd ...
 python run/run_all_test.py
 
 # cd module/login && py.test --junitxml results.xml test_login_200.py
