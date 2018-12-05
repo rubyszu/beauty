68cae4401a1b920ebaa646930b024b83a9f08c1c
ruby <rubyonly233@gmail.com>
2018-12-05 18:53:21 +0800

    pre_stage加到stage中

--- core/resolve_stage.py
+++ core/resolve_stage.py
@@ -10,7 +10,8 @@ def getStage(file):
 def resolveStage(stage):
 	#pre_stage
 	if "pre_stage" in stage:
-		pre_stage = resolveStage(stage.pre_stage)
+		pre_stage = getStage(stage.pre_stage)
+		stage.update({"pre_stage": pre_stage})
 	#save
 	request = stage.stage.request
 	response = stage.stage.response
